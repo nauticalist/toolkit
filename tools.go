881a4d3daed17d41571b2901d453ff3ecd62df68
nauticalist <iaksoy@seanapse.io>
2022-08-21 18:22:31 +0300

    added string slugify functionality

--- tools.go
+++ tools.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
@@ -162,4 +163,19 @@ func (t *Tools) CreateDirIfNotExists(path string) error {
 	}
 
 	return nil
+}
+
+func (t *Tools) Slugify(s string) (string, error) {
+	if s == "" {
+		return "", errors.New("empty string not permitted")
+	}
+
+	var re = regexp.MustCompile(`[^a-z\d]+`) 
+
+	slug := strings.Trim(re.ReplaceAllString(strings.ToLower(s), "-"), "-")
+	if len(slug) == 0 {
+		return "", errors.New("after removing characters slug is zero length")
+	}
+
+	return slug, nil
 }
